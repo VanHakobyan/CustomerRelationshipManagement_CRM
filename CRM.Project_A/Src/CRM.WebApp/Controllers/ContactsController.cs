120b4c2a8b8a6eca35b1a2d53dd9fe2934ba08c4
vanhakobyan <van19962013@mail.ru>
2017-05-23 14:35:23 +0400

    add controllers :three:

---
+++ CRM.Project_A/Src/CRM.WebApp/Controllers/ContactsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using EntityLibrary;
+
+namespace CRM.WebApp.Controllers
+{
+    public class ContactsController : ApiController
+    {
+        private DataBaseCRMEntities db = new DataBaseCRMEntities();
+
+        // GET: api/Contacts
+        public IQueryable<Contact> GetContacts()
+        {
+            return db.Contacts;
+        }
+
+        // GET: api/Contacts/5
+        [ResponseType(typeof(Contact))]
+        public IHttpActionResult GetContact(int id)
+        {
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(contact);
+        }
+
+        // PUT: api/Contacts/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutContact(int id, Contact contact)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != contact.ContactId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(contact).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContactExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Contacts
+        [ResponseType(typeof(Contact))]
+        public IHttpActionResult PostContact(Contact contact)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Contacts.Add(contact);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = contact.ContactId }, contact);
+        }
+
+        // DELETE: api/Contacts/5
+        [ResponseType(typeof(Contact))]
+        public IHttpActionResult DeleteContact(int id)
+        {
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            db.Contacts.Remove(contact);
+            db.SaveChanges();
+
+            return Ok(contact);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ContactExists(int id)
+        {
+            return db.Contacts.Count(e => e.ContactId == id) > 0;
+        }
+    }
+}
---
+++ CRM.Project_A/Src/CRM.WebApp/Controllers/TemplatesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using EntityLibrary;
+
+namespace CRM.WebApp.Controllers
+{
+    public class TemplatesController : ApiController
+    {
+        private DataBaseCRMEntities db = new DataBaseCRMEntities();
+
+        // GET: api/Templates
+        public IQueryable<Template> GetTemplates()
+        {
+            return db.Templates;
+        }
+
+        // GET: api/Templates/5
+        [ResponseType(typeof(Template))]
+        public IHttpActionResult GetTemplate(int id)
+        {
+            Template template = db.Templates.Find(id);
+            if (template == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(template);
+        }
+
+        // PUT: api/Templates/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTemplate(int id, Template template)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != template.TemplateId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(template).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TemplateExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Templates
+        [ResponseType(typeof(Template))]
+        public IHttpActionResult PostTemplate(Template template)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Templates.Add(template);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = template.TemplateId }, template);
+        }
+
+        // DELETE: api/Templates/5
+        [ResponseType(typeof(Template))]
+        public IHttpActionResult DeleteTemplate(int id)
+        {
+            Template template = db.Templates.Find(id);
+            if (template == null)
+            {
+                return NotFound();
+            }
+
+            db.Templates.Remove(template);
+            db.SaveChanges();
+
+            return Ok(template);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TemplateExists(int id)
+        {
+            return db.Templates.Count(e => e.TemplateId == id) > 0;
+        }
+    }
+}
