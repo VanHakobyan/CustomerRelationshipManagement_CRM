98a25de04c535679825d1e9a832bf74a2c92c150
Khachik Sukiasyan <khachatur125@gmail.com>
2017-05-26 00:28:06 +0400

    Fixed Excel/CSV parser

--- CRM.Project_A/Src/HelperMethods/Parser.cs
+++ CRM.Project_A/Src/HelperMethods/Parser.cs
@@ -3,10 +3,88 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LinqToExcel;
+using System.IO;
+using EntityLibrary;
 
 namespace HelperMethods
 {
     public class Parser
     {
+        public static List<ResultType> ParseExcel<ResultType>(byte[] array)
+        {
+            File.WriteAllBytes(@"D:\TestExel.xlsx", array);
+        
+            var pathToExcelFile = @"D:\TestExel.xlsx";
+            var sheetName = @"Sheet";
+            var excelFile = new ExcelQueryFactory(pathToExcelFile);
+
+            List<Row> rowsList = null;
+            try
+            {
+                rowsList = excelFile.Worksheet(sheetName).ToList();
+            }
+            catch (Exception)
+            {
+                File.Delete(@"D:\TestExel.xlsx");
+                File.WriteAllBytes(@"D:\TestExel.csv", array);
+                pathToExcelFile = @"D:\TestExel.csv";
+                excelFile = new ExcelQueryFactory(pathToExcelFile);
+                rowsList = excelFile.Worksheet(sheetName).ToList();
+            }
+
+            if (typeof(ResultType) == typeof(Contact))
+            {
+                var listOfContacts =
+                rowsList.Select(
+                contact =>
+                 new Contact
+                 {
+                    ContactId = int.Parse(contact["ContactId"]),
+                    FullName = contact["FullName"],
+                    CompanyName = contact["CompanyName"],
+                    Position = contact["Position"],
+                    Country = contact["Country"],
+                    Email = contact["Email"]
+                }) as List<ResultType>;
+
+                return listOfContacts;
+            }
+            else
+              if (typeof(ResultType) == typeof(EmailList))
+            {
+                var listOfContacts =
+                rowsList.Select(
+                emailList =>
+                 new EmailList
+                 {
+                     EmailListID = int.Parse(emailList["EmailListID"]),
+                     EmailListName = emailList["CompanyName"],
+                 }) as List<ResultType>;
+
+                return listOfContacts;
+            }
+            else
+            {
+                if (typeof(ResultType) == typeof(Template))
+                {
+                    var listOfContacts =
+                    rowsList.Select(
+                    template =>
+                     new Template
+                     {
+                         TemplateId = int.Parse(template["TemplateId"]),
+                         TemplateName = template["TemplateName"],
+                     }) as List<ResultType>;
+
+                    return listOfContacts;
+                }
+            }
+
+            return null;
+
+        }
+
+
     }
 }
