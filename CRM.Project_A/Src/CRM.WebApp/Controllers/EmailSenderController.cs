b25f1ee6d83fbfc79aeacc6aa7ac0f097b746e82
Tsovinar Ghazaryan <tsovinar.ghazaryan@yahoo.com>
2017-05-26 18:10:05 +0400

    add email sender not finally :one::six:

--- CRM.Project_A/Src/CRM.WebApp/Controllers/EmailSenderController.cs
+++ CRM.Project_A/Src/CRM.WebApp/Controllers/EmailSenderController.cs
@@ -9,6 +9,34 @@ namespace CRM.WebApp.Controllers
 {
     public class EmailSenderController : ApiController
     {
+        public static void SendEmail(string emailaddress)//List<Contact> list)
+        {
+            using (MailMessage msg = new MailMessage())
+            {
+                msg.From = new MailAddress("tsovinar.ghazarian@gmail.com");
+                msg.To.Add(emailaddress);
+                msg.Subject = "Heloo API";
+                msg.Body = "Ba chimacar";
+                SmtpClient client =
+                new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential("tsovinar.ghazarian@gmail.com", "123345667899")
+                };
 
+                try
+                {
+                    client.Send(msg);
+                }
+                catch (Exception ex)
+                {
+                    throw;
+               
+                }
+            }
+        }
     }
 }
