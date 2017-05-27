fe773e91417fe46bfef98bf47c21779c9c882cb1
Khachik Sukiasyan <khachatur125@gmail.com>
2017-05-27 16:25:21 +0400

    Added templates

--- CRM.Project_A/Src/CRM.WebApp/Infrastructure/EmailProvider.cs
+++ CRM.Project_A/Src/CRM.WebApp/Infrastructure/EmailProvider.cs
@@ -6,7 +6,9 @@ using System.Net;
 using System.Net.Mail;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Web;
+using System.IO;
 
 namespace CRM.WebApp.Infrastructure
 {
@@ -47,6 +49,69 @@ namespace CRM.WebApp.Infrastructure
             }
            
         }
+
+
+        private EmailContent GetEmailTemplate(Template template,Contact contact)
+        {
+            StringBuilder pathHtML = new StringBuilder( @"../../Templates/");
+            StringBuilder pathImage = new StringBuilder(@"../../Templates/images");
+            string contentHTML;
+            byte[] contentImage;
+            StringBuilder builder = null;
+            switch (template)
+            {
+                case Template.anniversary:
+                    {
+                        pathHtML.Append("anniversary.html");
+                        pathImage.Append("anniversary.jpg");
+                    }
+
+                    break;
+                case Template.birthday:
+                    {
+                        pathHtML.Append("birthday.html");
+                        pathImage.Append("birthday.jpg");
+                    }
+                    break;
+                case Template.christmas:
+                    {
+                        pathHtML.Append("christmas.html");
+                        pathImage.Append("christmas.jpg");
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            contentHTML = File.ReadAllText(pathHtML.ToString());
+            builder = new StringBuilder(contentHTML);
+
+            builder = builder.Replace("{FullName}", contact.FullName);
+            builder = builder.Replace("{CompanyName}", contact.CompanyName);
+            builder = builder.Replace("{Position}", contact.Position);
+            builder = builder.Replace("{Country}", contact.Country);
+            builder = builder.Replace("{DateTimeNow}", DateTime.Now.ToString("MMMM dd, yyyy"));
+
+            contentImage = System.IO.File.ReadAllBytes(pathImage.ToString());
+
+            return new EmailContent()
+            {
+                html = builder.ToString(),
+                Image = contentImage
+            };
+        }
        
+    }
+
+    public class EmailContent
+    {
+       public string html;
+       public byte[] Image;
+    }
+    public enum Template
+    {
+        anniversary,
+        birthday,
+        christmas
     }
 }
