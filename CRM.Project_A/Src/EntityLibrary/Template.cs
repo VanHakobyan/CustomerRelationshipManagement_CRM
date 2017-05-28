9885fa463d71bed8f92f069fdc8bc68d670a2b46
VanHakobyan <van19962013@mail.ru>
2017-05-28 17:49:36 +0400

    add path of html file :v:

--- CRM.Project_A/Src/EntityLibrary/Template.cs
+++ CRM.Project_A/Src/EntityLibrary/Template.cs
@@ -16,5 +16,6 @@ namespace EntityLibrary
     {
         public int TemplateId { get; set; }
         public string TemplateName { get; set; }
+        public string FilePath { get; set; }
     }
 }
