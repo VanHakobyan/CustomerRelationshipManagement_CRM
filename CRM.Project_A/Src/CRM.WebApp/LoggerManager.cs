0e7460d81bade16b61e94206b62db1665e5a151a
vanhakobyan <van19962013@mail.ru>
2017-05-31 19:13:23 +0400

    add log nuged and class manager :v:

---
+++ CRM.Project_A/Src/CRM.WebApp/LoggerManager.cs
@@ -0,0 +1,32 @@
+﻿using NLog;
+using NLog.Targets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace CRM.WebApp
+{
+    public class LoggerManager
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        public LoggerManager()
+        {
+            FileTarget loggerTarget = (FileTarget)LogManager.Configuration.FindTargetByName("file");
+            loggerTarget.DeleteOldFileOnStartup = false;
+        }
+        public void LogInfo(HttpMethod request, Uri uri)
+        {
+            Logger.Info($"Request: [ {request} ] | URL [ {uri} ]");
+        }
+        public void LogError(Exception ex, HttpMethod request, Uri uri)
+        {
+            Logger.Error($"\nRequest: [ {request} ] | URL [ {uri} ]\nErr: [ {ex.Message} ] Inner: [ {ex.InnerException?.Message} ]\n" + new string('-', 120));
+        }
+        public void LogException(Exception ex)
+        {
+            Logger.Log(LogLevel.Fatal, ex, $"\nErr: {ex.Message}\nInner: {ex.InnerException?.Message}\n");
+        }
+    }
+}
