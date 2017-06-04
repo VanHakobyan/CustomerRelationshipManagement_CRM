bd256e602c8b101968d7bab3ea36d30466b0a979
VanHakobyan <van19962013@mail.ru>
2017-06-04 23:08:51 +0400

    add logs

--- CRM.Project_A/Src/CRM.WebApp/Logs/LoggerManager.cs
+++ CRM.Project_A/Src/CRM.WebApp/Logs/LoggerManager.cs
@@ -2,6 +2,7 @@
 using NLog.Targets;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Web;
@@ -10,7 +11,7 @@ namespace CRM.WebApp
 {
     public class LoggerManager
     {
-        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
         public LoggerManager()
         {
             FileTarget loggerTarget = (FileTarget)LogManager.Configuration.FindTargetByName("file");
@@ -18,15 +19,31 @@ namespace CRM.WebApp
         }
         public void LogInfo(HttpMethod request, Uri uri)
         {
-            Logger.Info($"Request: [ {request} ] | URL [ {uri} ]");
+            logger.Info($"Request: [ {request} ] | URL [ {uri} ]");
         }
         public void LogError(Exception exception, HttpMethod request, Uri uri)
         {
-            Logger.Error($"\nRequest: [ {request} ] | URL [ {uri} ]\nErr: [ {exception.Message} ] Inner: [ {exception.InnerException?.Message} ]\n" + new string('-', 120));
+            logger.Error($"\nRequest: [ {request} ] | URL [ {uri} ]\nErr: [ {exception.Message} ] Inner: [ {exception.InnerException?.Message} ]\n" + new string('-', 120));
         }
         public void LogException(Exception ex)
         {
-            Logger.Log(LogLevel.Fatal, ex, $"\nErr: {ex.Message}\nInner: {ex.InnerException?.Message}\n");
+            logger.Log(LogLevel.Fatal, ex, $"\nErr: {ex.Message}\nInner: {ex.InnerException?.Message}\n");
+        }
+        public string ReadLogErrorData()
+        {
+            var fileTarget = (FileTarget)LogManager.Configuration.FindTargetByName("file");
+            var logEventInfo = new LogEventInfo { TimeStamp = DateTime.Now };
+            string fileName = fileTarget.FileName.Render(logEventInfo);
+            if (!File.Exists(fileName))
+                File.Create($"{logEventInfo.TimeStamp}.log");
+            var data = File.ReadAllLines(fileName);
+            string path = System.Web.HttpContext.Current?.Request.MapPath("~//Templates//LogErrors.html");
+            var html = File.ReadAllText(path);
+            string res = "";
+            foreach (string s in data)
+                res += s + "</br>";
+            var t = html.Replace("{data}", res).Replace("{filename}", fileName);
+            return t;
         }
     }
 }
